2bf06cacdd85852abffe39cd2144c10ce401ad47
oko1o <okolomanov@gmail.com>
2024-04-15 19:53:55 -0400

    hw07 v1.4

--- js/task-4.js
+++ js/task-4.js
@@ -1,14 +1,12 @@
 const loginForm = document.querySelector("form.login-form");
 
-console.log(loginForm);
-
 loginForm.addEventListener("submit", handleSubmit)
 
 function handleSubmit(event) {
     event.preventDefault();
     const form = event.target;
-    const email = form.elements.email.value;
-    const password = form.elements.password.value;
+    const email = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
 
     if (email === "" || password === "") {
         alert('All form fields must be filled in');
